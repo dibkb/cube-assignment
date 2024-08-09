22ea4963c5c755670ac6373163c9925b7f952c4e
dibkb <dibas9110@gmail.com>
2024-08-10 01:35:47 +0530

    customer list lazy loading

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -2,11 +2,47 @@
 import "../styles/customer.css";
 import { useCustomerContext } from "../hooks/useCustomerContext";
 import { Customer } from "./Customer";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const CustomerList = () => {
+  const customerListRef = useRef<HTMLDivElement>(null);
   const { customer } = useCustomerContext();
-  const renderCustomers = customer.map((cust) => (
-    <Customer key={cust.name} {...cust} />
-  ));
-  return <div className="customer-list-container">{renderCustomers}</div>;
+  // store the visible customers
+  const [visibleCustomers, setVisibleCustomers] = useState(
+    customer.slice(0, 10)
+  );
+  const [hasMore, setHasMore] = useState(visibleCustomers.length === 10);
+  const loadMoreCustomers = useCallback(() => {
+    const currLength = visibleCustomers.length;
+    const nextBatch = customer.slice(currLength, currLength + 10);
+    if (currLength + nextBatch.length >= customer.length) {
+      setHasMore(false);
+    }
+    setVisibleCustomers((prev) => [...prev, ...nextBatch]);
+  }, [customer, visibleCustomers]);
+  useEffect(() => {
+    const curr = customerListRef.current;
+    if (curr) {
+      const handleScroll = () => {
+        if (
+          curr.scrollTop + curr.clientHeight >= curr.scrollHeight - 1 &&
+          hasMore
+        ) {
+          loadMoreCustomers();
+        }
+      };
+      curr.addEventListener("scroll", handleScroll);
+      // cleanup
+      return () => {
+        curr.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [visibleCustomers.length, hasMore, loadMoreCustomers]);
+  return (
+    <div className="customer-list-container" ref={customerListRef}>
+      {visibleCustomers.map((cust) => (
+        <Customer key={cust.name} {...cust} />
+      ))}
+    </div>
+  );
 };
