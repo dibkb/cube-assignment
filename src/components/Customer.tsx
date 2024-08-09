e86b9a24df1a444146a5cd0001a4337ccb18c9cb
dibkb <dibas9110@gmail.com>
2024-08-09 23:47:25 +0530

    hover and onselect style

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -4,12 +4,17 @@ import "../styles/customer.css";
 import { type Customer as CustomerType } from "../types/Customer";
 export const Customer = (customer: CustomerType) => {
   const { title, name } = customer;
-  const { setSelectedCustomer } = useCustomerContext();
+  const { setSelectedCustomer, selectedCustomer } = useCustomerContext();
+  if (customer.name === selectedCustomer) {
+    return (
+      <div className="customer-card customer-card-selected">
+        <h1 className="customer-name">{name}</h1>
+        <p className="customer-title">{title}</p>
+      </div>
+    );
+  }
   return (
-    <div
-      className="customer-card customer-card-selected"
-      onClick={() => setSelectedCustomer(name)}
-    >
+    <div className="customer-card" onClick={() => setSelectedCustomer(name)}>
       <h1 className="customer-name">{name}</h1>
       <p className="customer-title">{title}</p>
     </div>
