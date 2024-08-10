165032c946da85b8d70e57cedf257a1c6f8110a7
dibkb <dibas9110@gmail.com>
2024-08-10 16:35:45 +0530

    mobile screen scroll bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
-// css file
 import "./styles/App.css";
 import { CustomerList } from "./components/CustomerList";
 import { SelectedCustomer } from "./components/SelectedCustomer";
+
 function App() {
   const [isLeftSectionOpen, setLeftSectionOpen] = useState(false);
-  // toggle the left section
+
+  // toggle handler
   const toggleSidebar = () => {
     setLeftSectionOpen((prevState) => !prevState);
   };
-  // onclose left section
+
+  // on close handler
   const closeLeftSection = () => {
     setLeftSectionOpen(false);
   };
+
   return (
     <main className="main-container">
       <h1 className="heading">Cube Assignment</h1>
-      <section className="main-grid" data-testid="main-grid">
-        <div
-          className={`left-section ${isLeftSectionOpen ? "open" : ""}`}
-          data-testid="left-section"
-        >
+      <section
+        className={`main-grid ${isLeftSectionOpen ? "left-open" : ""}`}
+        data-testid="main-grid"
+      >
+        <div className="left-section" data-testid="left-section">
           <CustomerList onClickCallback={closeLeftSection} />
         </div>
         <div className="right-section" data-testid="right-section">
           <SelectedCustomer />
         </div>
-        <button className="toggle-button" onClick={toggleSidebar}>
-          {isLeftSectionOpen ? "Close" : "Open"} Left
-        </button>
       </section>
+      <button className="toggle-button" onClick={toggleSidebar}>
+        {isLeftSectionOpen ? "Close" : "Open"} Left
+      </button>
     </main>
   );
 }
